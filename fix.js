5c75c6878c8d65f93ba9debc3a0dc3000cbcde6f
Chivyyyyy7117 <s135003@student.squ.edu.om>
2025-11-20 18:41:51 +0400

    Add files via upload

---
+++ fix.js
@@ -0,0 +1,4 @@
+// fix.js - A simple script to demonstrate XSS.
+// In a real attack, this file would contain code to steal cookies or other data.
+
+alert('XSS Successful! If this were a real attack, your data could now be exfiltrated.');
